2c848308a18695cefcf4ebb1ed16dacabe7b4882
Chung-Wen Liao <ljw830517@gmail.com>
2017-03-31 17:07:44 -0400

    [Add] Online Training

--- Test_Davis.py
+++ Test_Davis.py
@@ -14,6 +14,51 @@ from PIL import Image
 
 from config import *
 
+def onlineTraining():
+    # Create the model
+    fcn = Davis_FCN.FCN() 
+    x = tf.placeholder(tf.float32) #shape=[batch size, dimemsionality] 
+    y_ = tf.placeholder(tf.float32)
+    y = fcn.build(x, train=True, num_classes=NUM_CLASSES, debug=True)
+
+	# Define loss and optimizer
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+    #cross_entropy = tf.reduce_sum(cross_entropy)
+    #train_step = tf.train.GradientDescentOptimizer(LR).minimize(cross_entropy)
+    train_step = tf.train.MomentumOptimizer(learning_rate=LR, momentum=MOMENTUM)
+    train_step = train_step.minimize(cross_entropy)
+
+    # Session Define
+    sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                            log_device_placement=False))
+    saver = tf.train.Saver()
+    if file_index != 0:
+        saver.restore(sess, "./models/model%s"%MODEL_INDEX)
+        print("Model restored ...")
+    
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    
+    # Training
+    print('='*40)
+    print('Training ...')
+    loss = []
+    for i in range(MAX_ITER):
+        batch_xs, batch_ys, filename = pascal_reader.next_batch(BATCH_SIZE)
+        _, loss_val = sess.run([train_step,cross_entropy], feed_dict={x: batch_xs, y_: batch_ys})
+        save_path = saver.save(sess, "./models/model%s"%MODEL_INDEX)      
+        loss.append(loss_val)
+        f.write(str(file_index+i+1)+'\n')
+        log = 'Iteration: %s'%str(i) + ' | Filename: %s'%filename + \
+                ' | Model saved in file: %s'%save_path + ' | Cross entropy loss: %s'%str(loss_val)
+        logs.write(log+'\n')
+        print(log)
+    logs.close()
+    f.close()
+    np.save('./models/trCrossEntropyLoss%s'%MODEL_INDEX, np.array(loss))
+
+
 def main(argv):
 	
     # import data
@@ -22,8 +67,9 @@ def main(argv):
     # Create the model
     fcn = Davis_FCN.FCN()
     x = tf.placeholder(tf.float32) #shape=[batch size, dimemsionality] 
-    y_ = tf.placeholder(tf.float32)
+    y_ = tf.placeholder(tf.float32) 
     y = fcn.build(x, num_classes=NUM_CLASSES, debug=False)
+    
 
     # Define prediction
     correct_prediction = tf.equal(tf.argmax(y, 3), tf.argmax(y_, 3))
