9fe255f2b03e664bfced10afaa6f61d87741686d
uniericuni <uniericuni@gmail.com>
2017-04-03 22:14:53 -0400

    [Modify] split .m files to ./matlab/

--- epicflow.py
+++ epicflow.py
@@ -1,7 +1,9 @@
 import sys
 sys.path.append( '/home/erhsin/lib' )
-import matlab.engine
 
-matlab_eng = matlab.engine.start_matlab()
+import matlab.engine as mateng
+
+matlab_eng = mateng.start_matlab()
+matlab_eng.addpath('./matlab')
 out = matlab_eng.epicflow('../1.jpg', '../2.jpg')
 print out.split('$')
---
+++ matlab/tmp.m
@@ -0,0 +1,4 @@
+function [out1,out2] = tmp()
+
+    out1 = 1;
+    out2 = [2,3;4,5];
